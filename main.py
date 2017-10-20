3a5d3eb1b52e075e714be85ef7efdb5cf295f0e1
Blake Tsuhako <blaket@grow.com>
2017-10-19 20:04:30 -0600

    inital main and tests
    for the win!

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import argparse
+# import numpy
+
+
+def sumMax(args):
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=float, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+    parsedArgs = parser.parse_args(args)
+    print(parsedArgs.accumulate(parsedArgs.integers))
+    return parsedArgs.accumulate(parsedArgs.integers)
+
+
+if __name__ == "__main__":
+    sumMax(sys.argv[1:])
---
+++ test_main_unittest.py
@@ -0,0 +1,24 @@
+import unittest
+from main import sumMax
+
+
+class TestSumMax(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_max_3_4_7(self):
+        self.assertEqual(sumMax('3 4 7'.split()), 7)
+
+    def test_max_n3_4_2(self):
+        self.assertEqual(sumMax('-3 4 2'.split()), 4)
+
+    def test_sum_3_4_8(self):
+        self.assertEqual(sumMax('3 4 8 --sum'.split()), 15)
+
+    def test_sum_3_4_8p2(self):
+        self.assertEqual(sumMax('3 4 8.5 --sum'.split()), 15.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
